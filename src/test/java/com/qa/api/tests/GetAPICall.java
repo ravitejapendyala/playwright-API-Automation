6b7785337d139d402f7d1b199d3d823db83d2ea1
ravitejapendyala <raviteja.pendyalas@gmail.com>
2024-01-24 14:02:38 +0530

    First commit with Playwright references added in pom.xml

---
+++ src/test/java/com/qa/api/tests/GetAPICall.java
@@ -0,0 +1,35 @@
+package com.qa.api.tests;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.playwright.APIRequest;
+import com.microsoft.playwright.APIRequestContext;
+import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.Playwright;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GetAPICall {
+
+    @Test
+    public void getUsersAPITest() throws IOException {
+
+        Playwright playwright =  Playwright.create();
+        APIRequest request =  playwright.request();
+        APIRequestContext requestContext =  request.newContext();
+        APIResponse apiResponse =  requestContext.get("https://gorest.co.in/public/v2/users");
+        System.out.println("Response status code is : "+apiResponse.status());
+        System.out.println("Response status code is : "+apiResponse.statusText());
+        System.out.println("Response body is : "+apiResponse.body());
+        Assert.assertEquals(apiResponse.status(),200);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonResponse =  objectMapper.readTree(apiResponse.body());
+        System.out.println("Response body is: ");
+        System.out.println(jsonResponse.toPrettyString());
+        System.out.println("API Response url:  "+apiResponse.url());
+        System.out.println("Headers are : "+ apiResponse.headersArray().toString());
+
+    }
+}
