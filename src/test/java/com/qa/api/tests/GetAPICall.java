0f7115fde566d1c00e3f03a56e56284282ea5d93
ravitejapendyala <raviteja.pendyalas@gmail.com>
2024-01-25 23:11:05 +0530

    test api with query parameter

--- src/test/java/com/qa/api/tests/GetAPICall.java
+++ src/test/java/com/qa/api/tests/GetAPICall.java
@@ -6,20 +6,29 @@ import com.microsoft.playwright.APIRequest;
 import com.microsoft.playwright.APIRequestContext;
 import com.microsoft.playwright.APIResponse;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.RequestOptions;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class GetAPICall {
+    Playwright playwright;
+    APIRequest request;
+    APIRequestContext requestContext;
+    @BeforeTest
+    public  void beforeTest(){
+         playwright =  Playwright.create();
+         request =  playwright.request();
+            requestContext =  request.newContext();
+    }
 
     @Test
     public void getUsersAPITest() throws IOException {
 
-        Playwright playwright =  Playwright.create();
-        APIRequest request =  playwright.request();
-        APIRequestContext requestContext =  request.newContext();
+
         APIResponse apiResponse =  requestContext.get("https://gorest.co.in/public/v2/users");
         System.out.println("Response status code is : "+apiResponse.status());
         System.out.println("Response status code is : "+apiResponse.statusText());
@@ -35,5 +44,28 @@ public class GetAPICall {
         System.out.println("Headers are : "+ apiResponse.headersArray().toString());
         System.out.println("Headers are : "+ headers.toString());
         Assert.assertEquals("application/json;",headers.get("content-type"));
+    }
+    @Test
+    public void getUsersWithParameterAPITest() throws IOException {
+
+
+        APIResponse apiResponse =  requestContext.get("https://gorest.co.in/public/v2/users",
+                RequestOptions.create()
+                        .setQueryParam("id",6007021)
+                        .setQueryParam("status","inactive"));
+        System.out.println("Response status code is : "+apiResponse.status());
+        System.out.println("Response status code is : "+apiResponse.statusText());
+        System.out.println("Response body is : "+apiResponse.body());
+        System.out.println("Response with plain text is : "+apiResponse.text());
+        Assert.assertEquals(apiResponse.status(),200);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonResponse =  objectMapper.readTree(apiResponse.body());
+        System.out.println("Response body is: ");
+        System.out.println(jsonResponse.toPrettyString());
+        System.out.println("API Response url:  "+apiResponse.url());
+        Map<String,String> headers = apiResponse.headers();
+        System.out.println("Headers are : "+ apiResponse.headersArray().toString());
+        System.out.println("Headers are : "+ headers.toString());
+
     }
 }
