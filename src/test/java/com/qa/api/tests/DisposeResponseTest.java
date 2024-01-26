18f371bdb9448496684f86cfd162cbb0ca2ec0f6
ravitejapendyala <raviteja.pendyalas@gmail.com>
2024-01-26 23:57:14 +0530

    Dispose response validations

--- src/test/java/com/qa/api/tests/DisposeResponseTest.java
+++ src/test/java/com/qa/api/tests/DisposeResponseTest.java
@@ -2,10 +2,7 @@ package com.qa.api.tests;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.microsoft.playwright.APIRequest;
-import com.microsoft.playwright.APIRequestContext;
-import com.microsoft.playwright.APIResponse;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.RequestOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -39,9 +36,22 @@ public class DisposeResponseTest {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonReponse =  objectMapper.readTree(apiResponse.body());
         System.out.println("Pretty response is : "+jsonReponse.toPrettyString());
+        String status = apiResponse.statusText();
+        System.out.println("Status before response dispose  : "+status);
 
-        apiResponse.dispose();
-        System.out.println("Pretty response After dispose is : "+apiResponse.text());
+        apiResponse.dispose(); // Will dispose only response body but response text , status ,url will remain intact
+        try{
+            System.out.println("Pretty response After dispose is : "+apiResponse.text());
+
+        }
+        catch (PlaywrightException ex){
+            System.out.println("Response is disposed");
+        }
+        String status_AfterDisponse = apiResponse.statusText();
+        System.out.println("Status after response dispose  : "+status_AfterDisponse);
+        System.out.println("Response URL after   response dispose  : "+apiResponse.url());
+        System.out.println("Response code after   response dispose  : "+apiResponse.status());
+        //System.out.println("Pretty response After dispose is : "+apiResponse.text());
 
     }
     @Test
