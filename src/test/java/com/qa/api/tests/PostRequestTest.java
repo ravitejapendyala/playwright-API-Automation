f8adc0597d2da5dc8cd527760e7c37bc8f8a10a4
ravitejapendyala <raviteja.pendyalas@gmail.com>
2024-01-31 18:39:30 +0530

    Adding post request for creating user

--- src/test/java/com/qa/api/tests/PostRequestTest.java
+++ src/test/java/com/qa/api/tests/PostRequestTest.java
@@ -7,14 +7,14 @@ import com.microsoft.playwright.APIRequestContext;
 import com.microsoft.playwright.APIResponse;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.HttpHeader;
+import com.microsoft.playwright.options.RequestOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PostRequestTest {
     Playwright playwright;
@@ -31,25 +31,20 @@ public class PostRequestTest {
     public void PostRequestTest() throws IOException {
 
 
-        APIResponse apiResponse =  requestContext.get("https://gorest.co.in/public/v2/users");
-        System.out.println("Response status code is : "+apiResponse.status());
-        System.out.println("Response status code is : "+apiResponse.statusText());
-        System.out.println("Response body is : "+apiResponse.body());
-        System.out.println("Response with plain text is : "+apiResponse.text());
-        Assert.assertEquals(apiResponse.status(),200);
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonResponse =  objectMapper.readTree(apiResponse.body());
-        //System.out.println("Response body is: ");
-        //System.out.println(jsonResponse.toPrettyString());
-        System.out.println("API Response url:  "+apiResponse.url());
-        Map<String,String> headers = apiResponse.headers();
-        headers.forEach((k,v)-> System.out.println("Header Name: "+k+ " With Value: "+v));
-        System.out.println("Total response headers : "+headers.size());
-//        System.out.println("Headers are : "+ apiResponse.headersArray().toString());
-//        System.out.println("Headers are : "+ headers.toString());
-        Assert.assertEquals("application/json; charset=utf-8",headers.get("content-type"));
-        Assert.assertEquals("cloudflare",headers.get("server"));
-        List<HttpHeader> headerslist = apiResponse.headersArray();
+        Map<String,Object> inputData = new HashMap<String, Object>();
+        inputData.put("name","Ravi Teja Pendyala");
+        inputData.put("gender","male");
+        inputData.put("email","tenali.ramakrishna3@17ce.com");
+        inputData.put("status","active");
+        APIResponse apiPostResponse =  requestContext.post("https://gorest.co.in/public/v2/users",
+                RequestOptions.create()
+                .setHeader("Content-Type","application/json")
+                .setHeader("Authorization","Bearer 7bb2b7149e52ec7745208dd33822ad09a8117980860506d564ed566d5b12c4aa")
+                .setData(inputData));
+        System.out.println("Response status code is : "+apiPostResponse.status());
+        Assert.assertEquals(apiPostResponse.status(),201);
+
+        List<HttpHeader> headerslist = apiPostResponse.headersArray();
         for(HttpHeader h: headerslist){
             System.out.println("Header -> "+h.name+" Value-> "+h.value);
         }
